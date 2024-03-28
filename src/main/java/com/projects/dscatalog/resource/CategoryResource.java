631fa3d49a78d986db1ace93739bc83d87f02b16
JJ <jONATHA949@GMAI.COM>
2024-03-28 17:57:50 -0300

    DTO

--- src/main/java/com/projects/dscatalog/resource/CategoryResource.java
+++ src/main/java/com/projects/dscatalog/resource/CategoryResource.java
@@ -1,5 +1,6 @@
 package com.projects.dscatalog.resource;
 
+import com.projects.dscatalog.dto.CategoryDTO;
 import com.projects.dscatalog.entity.Category;
 import com.projects.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,8 @@ public class CategoryResource {
     @Autowired
     private CategoryService service;
     @GetMapping
-    public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll(){
+        List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/projects/dscatalog/services/CategoryService.java
+++ src/main/java/com/projects/dscatalog/services/CategoryService.java
@@ -1,18 +1,23 @@
 package com.projects.dscatalog.services;
 
+import com.projects.dscatalog.dto.CategoryDTO;
 import com.projects.dscatalog.entity.Category;
 import com.projects.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     @Autowired
-   private CategoryRepository categoryRepository;
-   public List<Category> findAll(){
-       return categoryRepository.findAll();
+    private CategoryRepository repository;
 
-   }
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
 }
